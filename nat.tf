d957bd5deec97f1390da6ea1ba8822f02b4b1992
Nishant <gangwarnishant020@gmail.com>
2024-10-13 14:16:39 +0530

    nat gateway nat.tf

---
+++ nat.tf
@@ -0,0 +1,32 @@
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "k8s-nat-gateway"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
