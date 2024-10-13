87f871c948a6294683cbf582b7ab835b0c1c4fa9
Nishant <gangwarnishant020@gmail.com>
2024-10-13 14:13:02 +0530

    created routes.tf

---
+++ routes.tf
@@ -0,0 +1,19 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.k8s_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0" # This allows all outbound traffic
+    gateway_id = aws_internet_gateway.k8s_igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
